52b3766770e836ecee0818b43008dac1e610863e
Fumnanya77 <gilbertgabriellafrances@gmail.com>
2024-03-26 09:19:59 -0400

    Configuration Management with Puppet, over commenting

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,17 @@
 #This is a manifest that installs a package in this case flask
 
-exec { 'install flask':
-  command => 'pip3 install flask==2.1.0',
-  path    => '/usr/bin',
-  unless  => 'pip3 list | grep flask',
+exec { 'apt-update':
+  command => '/usr/bin/apt-get update',
+  path    => '/usr/bin'
 }
+
+package { 'flask':
+  require  => Exec['apt-update'],
+  ensure   => '2.1.0',
+  provider => 'pip',
+}
+
+#exec { 'install flask':
+#  command => 'pip3 install flask==2.1.0',
+#  path    => '/usr/bin',
+#}
